f8ed8515f638b88a8a67fa6a697a4acfbe296910
LeviathanST <phamquochung170505@gmail.com>
2026-02-08 21:25:08 +0700

    render: add `process_render` schedule & render queue in the form of layers
    All renderable entity id are taken in the `update` schedule and
    added to render_fn in `RenderItem`, and then adding the item to
    `RenderQueue`. In `process_render`, all items in the queue are
    iterated and the render_fn are called.

--- src/common/components/rectangle.zig
+++ src/common/components/rectangle.zig
@@ -1,6 +1,10 @@
 const rl = @import("raylib");
+const ecs = @import("../../ecs.zig");
 
-const Position = @import("position.zig").Position;
+const World = ecs.World;
+const RenderQueue = @import("../../render.zig").RenderQueue;
+const Resource = ecs.query.Resource;
+const Transform = @import("transform.zig").Transform;
 const QueryToRender = @import("../utils.zig").QueryToRender;
 
 pub const Rectangle = struct {
@@ -9,15 +13,32 @@ pub const Rectangle = struct {
     color: rl.Color,
 };
 
-pub fn render(queries: QueryToRender(&.{ Position, Rectangle })) !void {
+pub fn render(w: *World, e_id: ecs.Entity.ID) !void {
+    const rec, const _transform =
+        try w
+            .entity(e_id)
+            .getComponents(&.{ Rectangle, Transform });
+
+    rl.drawRectangle(
+        _transform.x,
+        _transform.y,
+        rec.width,
+        rec.height,
+        rec.color,
+    );
+}
+
+pub fn addRenderToQueue(
+    queries: QueryToRender(&.{ Transform, ecs.Entity.ID, Rectangle }),
+    render_queue: Resource(*RenderQueue),
+) !void {
     for (queries.many()) |query| {
-        const pos, const rec = query;
-        rl.drawRectangle(
-            pos.x,
-            pos.y,
-            rec.width,
-            rec.height,
-            rec.color,
-        );
+        const transform, const entity_id, _ = query;
+
+        try render_queue.result.add(.{
+            .render_fn = render,
+            .entity_id = entity_id,
+            .depth = transform.z,
+        });
     }
 }
--- src/render.zig
+++ src/render.zig
@@ -3,6 +3,7 @@
 //! - Schedules: `startup`, `update`, `deinit` (see `RenderScheduleOrder` for the order)
 //!
 //! TODO: ordered rendering by depth values (2d-only)
+const std = @import("std");
 const ecs = @import("ecs.zig");
 const rl = @import("raylib");
 
@@ -22,9 +23,13 @@ pub const RenderSet = SystemSet{ .name = "render" };
 /// See `render.RenderSet` for non-UI components
 pub const UiRenderSet = SystemSet{ .name = "ui_render" };
 
-const schedules = struct {
+pub const schedules = struct {
     pub const startup = ScheduleLabel.init("startup");
+    /// Prepare needed information (render queue) for
+    /// the `process_render` schedule
     pub const update = ScheduleLabel.init("update");
+    /// This schedule draw all renderable component
+    pub const process_render = ScheduleLabel.init("process_render");
     pub const deinit = ScheduleLabel.init("deinit");
 };
 
@@ -33,18 +38,35 @@ const RenderScheduleOrder = struct {
     labels: []const ScheduleLabel = &[_]ScheduleLabel{
         schedules.startup,
         schedules.update,
+        schedules.process_render,
         schedules.deinit,
     },
 };
 
 // TODO: each render system will add component tuples to
 //       the queue.
-// pub const RenderItem = struct {
-//     render_fn: *anyopaque,
-//     depth: i32,
-// };
-// pub fn compareRI(ctx: void, a: RenderItem, b: RenderItem) std.math.Order {}
-// pub const RenderQueue = std.PriorityQueue(RenderItem, void, compareRI);
+pub const RenderItem = struct {
+    render_fn: *const fn (*World, ecs.Entity.ID) anyerror!void,
+    entity_id: ecs.Entity.ID,
+    depth: i32,
+};
+
+fn compareRI(ctx: void, a: RenderItem, b: RenderItem) std.math.Order {
+    _ = ctx;
+    if (a.depth == b.depth) {
+        return .eq;
+    } else if (a.depth < b.depth) {
+        return .lt;
+    } else if (a.depth > b.depth) {
+        return .gt;
+    } else {
+        unreachable;
+    }
+}
+
+/// This queue affects **2d components** in the form of layers.
+/// In 3D, this queue do nothing (the queue still process, but as usual).
+pub const RenderQueue = std.PriorityQueue(RenderItem, void, compareRI);
 
 fn render(
     w: *World,
@@ -56,9 +78,21 @@ fn render(
     rl.clearBackground(.white);
     defer rl.endDrawing();
 
-    for (orders.labels) |label| {
+    for (orders.labels) |label| { // Add items into the queue
         try w.render_scheduler.runSchedule(w.alloc, w, label);
     }
+}
+
+pub fn processRender(
+    w: *World,
+    render_queue: Resource(*RenderQueue),
+) !void {
+    const queue = render_queue.result;
+    var iter = queue.iterator();
+    while (iter.next()) |item| {
+        try item.render_fn(w, item.entity_id);
+    }
+    queue.clearAndFree(); // reset the queue
 }
 
 pub const schedule_mod = struct {
@@ -66,14 +100,17 @@ pub const schedule_mod = struct {
         _ = w
             .addSchedule(.render, schedules.startup)
             .addSchedule(.render, schedules.update)
+            .addSchedule(.render, schedules.process_render)
             .addSchedule(.render, schedules.deinit)
             .addResource(RenderScheduleOrder, .{})
+            .addResource(RenderQueue, .init(w.alloc, {}))
             .configureSet(
                 .render,
                 schedules.update,
                 UiRenderSet,
                 .{ .after = &.{RenderSet} },
             )
-            .addSystem(.render, Scheduler.entry, render);
+            .addSystem(.render, Scheduler.entry, render)
+            .addSystem(.render, schedules.process_render, processRender);
     }
 };

0ea7d60710a500396d20bf60a9c7e024a994945c
LeviathanST <phamquochung170505@gmail.com>
2026-02-15 18:49:07 +0700

    ecs: add `despawnEntity` & `Entity.despawnRecursive`

--- src/ecs/Entity.zig
+++ src/ecs/Entity.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const query_utils = @import("query/utils.zig");
 
 const Children = @import("hierarchy.zig").Children;
+const ChildOf = @import("hierarchy.zig").ChildOf;
 const World = @import("World.zig");
 
 const Entity = @This();
@@ -20,35 +21,59 @@ pub fn withChildren(
     try callback(self);
 }
 
-/// Spawn a new entity and return the id
-pub fn spawn(self: Entity, components: anytype) Entity {
-    const child_id =
-        self
-            .world
-            .spawnEntity(components)
-            .id;
+/// Despawn the caller and its children.
+pub fn despawnRecursive(self: Entity) !void {
+    despawn_children: {
+        const children_q = self.getComponents(&.{Children}) catch |err| switch (err) {
+            World.GetComponentError.ValueNotFound, World.GetComponentError.StorageNotFound => break :despawn_children,
+            else => return err,
+        };
+        const children: Children = children_q[0];
 
-    self.pushChildren(&[_]ID{child_id});
-    return .{ .id = child_id, .world = self.world };
+        for (children.child_ids.items) |child_id| {
+            const child_entity = Entity{ .id = child_id, .world = self.world };
+            try child_entity.despawnRecursive();
+        }
+    }
+
+    try self.despawn();
 }
 
-/// Push children's entity id as components for the parent entity
-pub fn pushChildren(
+pub inline fn despawn(self: Entity) !void {
+    try self.world.despawnEntity(self.id);
+}
+
+/// Spawn a new entity and return the id
+pub fn spawn(self: Entity, components: anytype) Entity {
+    const child =
+        self
+            .world
+            .spawnEntity(components);
+
+    _ = child.setComponent(ChildOf, .{ .parent_id = self.id });
+    self.pushChild(child.id);
+    return child;
+}
+
+/// Push a child's id as a component for the parent entity
+pub fn pushChild(
     self: Entity,
-    child_ids: []const ID,
+    child_id: ID,
 ) void {
     if (self.getComponents(&.{*Children})) |query| {
         const children: *Children = query[0];
-        children.ids.appendSlice(self.world.alloc, child_ids) catch @panic("OOM");
+        children.*.child_ids.append(self.world.alloc, child_id) catch @panic("OOM");
     } else |err| switch (err) {
         World.GetComponentError.ValueNotFound, World.GetComponentError.StorageNotFound => {
-            _ = self.setComponent(Children, .{ .ids = .empty });
+            // init empty first and then append the list to ensure
+            // all elements are allocated by the same allocator
+            _ = self.setComponent(Children, .{ .child_ids = .empty });
             const children: *Children =
                 (self
                     .getComponents(&.{*Children}) catch
                     @panic("OOM") // if this error occurs, maybe its from list allocations in `tuplesFromTypes`.
                 )[0];
-            children.ids.appendSlice(self.world.alloc, child_ids) catch @panic("OOM");
+            children.*.child_ids.append(self.world.alloc, child_id) catch @panic("OOM");
         },
         std.mem.Allocator.Error.OutOfMemory => @panic("OOM"),
     }
